5f6d763a961937ab04ec222030ae060e0ab1a8be
Andrey Sukhovev <andrei5s@yandex.ru>
2022-10-24 15:13:15 +0300

    Проектная работа 13

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ const cardsRouter = require('./cards');
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
+router.all('*', () => {
+    // throw new NotFoundError('Запрашиваемый ресурс не найден');
+    return res.status(404).send({
+        message: 'Запрашиваемый ресурс не найден',
+        err
+    });
+});
+
 module.exports = router;
