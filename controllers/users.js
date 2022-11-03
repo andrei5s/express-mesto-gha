5754ab1079561b569ab0735e0e0a8394083b52d6
Andrey Sukhovev <andrei5s@yandex.ru>
2022-11-03 21:24:19 +0300

    Проектная работа 14

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,12 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const ExistError = require('../errors/existerr');
 const BadRequestError = require('../errors/bedrequserror');
 const BadDataError = require('../errors/beddataerr');
-const NotFoundError = require('../errors/not-found-err');
+// const NotFoundError = require('../errors/not-found-err');
 const { STATUS_OK, STATUS_CREATED } = require('../utils/constants');
 
 const createUser = (req, res, next) => {
@@ -87,15 +88,26 @@ const getUser = (req, res, next) => {
     });
 }; */
 
-const getUserById = (req, res, next) => {
-  User.findById(req.params.id).select('+password')
+const getUserById = (req, res) => {
+/* User.findById(req.params.id).select('+password')
     .then((user) => {
       if (!user) {
         throw new NotFoundError('Пользователь не найден');
       }
       res.status(STATUS_OK).send(user);
     })
-    .catch(next);
+    .catch(next); */
+  User.findById(req.params.id).orFail(new Error('NotFound'))
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        return res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(400).send({ message: 'Не корректный _id' });
+      }
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+    });
 };
 
 const updateProfile = (req, res, next) => {
