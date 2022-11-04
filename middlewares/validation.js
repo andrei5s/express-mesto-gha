4cb43607f906352fc49347e42c0fd1e34595e4af
Andrey Sukhovev <andrei5s@yandex.ru>
2022-11-04 20:20:05 +0300

    Проектная работа 14

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,71 +1,71 @@
 const { celebrate, Joi } = require('celebrate');
 
 const checkUser = celebrate({
-    body: Joi.object().keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required().min(8),
-        name: Joi.string().min(2).max(30),
-        about: Joi.string().min(2).max(30),
-        // eslint-disable-next-line no-useless-escape
-        avatar: Joi.string().pattern(/https?:\/\/(www)?[\-\.~:\/\?#\[\]@!$&'\(\)*\+,;=\w]+#?\b/),
-    }),
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().pattern(/https?:\/\/(www)?[\-\.~:\/\?#\[\]@!$&'\(\)*\+,;=\w]+#?\b/),
+  }),
 });
 
 const checkLogin = celebrate({
-    body: Joi.object().keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required().min(8),
-    }),
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
 });
 
 const checkProfile = celebrate({
-    body: Joi.object().keys({
-        name: Joi.string().required().min(2).max(30),
-        about: Joi.string().required().min(2).max(30),
-    }),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
 });
 
 const checkAvatar = celebrate({
-    body: Joi.object().keys({
-        // eslint-disable-next-line no-useless-escape
-        avatar: Joi.string().required().pattern(/https?:\/\/(www)?[\-\.~:\/\?#\[\]@!$&'\(\)*\+,;=\w]+#?\b/),
-    }),
+  body: Joi.object().keys({
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().required().pattern(/https?:\/\/(www)?[\-\.~:\/\?#\[\]@!$&'\(\)*\+,;=\w]+#?\b/),
+  }),
 });
 
 const checkUserById = celebrate({
-    params: Joi.object().keys({
-        id: Joi.string().alphanum().length(24),
-    }),
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24),
+  }),
 });
 
 const checkNewCard = celebrate({
-    body: Joi.object().keys({
-        name: Joi.string().required().min(2).max(30),
-        // eslint-disable-next-line no-useless-escape
-        link: Joi.string().required().pattern(/https?:\/\/(www)?[\-\.~:\/\?#\[\]@!$&'\(\)*\+,;=\w]+#?\b/),
-    }),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
+    link: Joi.string().required().pattern(/https?:\/\/(www)?[\-\.~:\/\?#\[\]@!$&'\(\)*\+,;=\w]+#?\b/),
+  }),
 });
 
 const checkCardId = celebrate({
-    params: Joi.object().keys({
-        cardId: Joi.string().alphanum().length(24),
-    }),
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
 });
 
 const checkDeletedCardId = celebrate({
-    params: Joi.object().keys({
-        // id: Joi.string().alphanum().length(24),
-        cardId: Joi.string().alphanum().length(24),
-    }),
+  params: Joi.object().keys({
+    // id: Joi.string().alphanum().length(24),
+    cardId: Joi.string().alphanum().length(24),
+  }),
 });
 
 module.exports = {
-    checkUser,
-    checkLogin,
-    checkProfile,
-    checkAvatar,
-    checkUserById,
-    checkNewCard,
-    checkCardId,
-    checkDeletedCardId,
+  checkUser,
+  checkLogin,
+  checkProfile,
+  checkAvatar,
+  checkUserById,
+  checkNewCard,
+  checkCardId,
+  checkDeletedCardId,
 };
