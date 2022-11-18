ae87a913a88164cf9402cb2932dc89203e6c49cd
Andrey Sukhovev <andrei5s@yandex.ru>
2022-11-18 17:24:05 +0300

    Проектная работа 15

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,10 +3,7 @@ const allowedCors = [
   'http://project.andrei5s.nomoredomains.icu',
   'https://api.project.andrei5s.nomoredomains.icu',
   'http://api.project.andrei5s.nomoredomains.icu',
-  'https://aoi.project.andrei5s.nomoredomains.icu/users',
-  'https://api.project.andrei5s.nomoredomains.icu/users/me',
   'http://localhost:3000',
-  'http://localhost:3001',
 ];
 
 // eslint-disable-next-line consistent-return
@@ -14,6 +11,7 @@ const corsOption = ((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
