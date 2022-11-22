12824b4f8c55c93c4396b0bfeb3cd9eb83a6625d
Andrey Sukhovev <andrei5s@yandex.ru>
2022-11-22 13:41:51 +0300

    Проектная работа 15

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,12 +11,10 @@ module.exports = (req, res, next) => {
     return next(new BadDataError('Необходима авторизация'));
   }
 
-  // const token = extractBearerToken(authorization);
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    // payload = jwt.verify(token, 'super-strong-secret');
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new BadDataError('Необходима авторизация'));
